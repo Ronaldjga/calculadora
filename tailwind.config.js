49f451489002df337cf00cca0413e5116769b563
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-02-21 10:20:18 -0300

    project config

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}', './src/patterns/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    './src/patterns/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yelloPrimary: "#F2BF5E",
+        darkBluePrimary: "#022026",
+        grayPimary: "#022026",
+      },
+      fontFamily: {
+        sans: ['ui-sans-serif', 'system-ui',],
+        serif: ['ui-serif', 'Georgia',],
+        mono: ['ui-monospace', 'SFMono-Regular',],
+        h1Title: ['Cinzel',],
+        h2Title: ['"Domine"',],
+        body: ['"Work Sans"',],
+      }
+    },
+  },
+  plugins: [],
+}
