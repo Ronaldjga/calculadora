6d0ac4d30b7b6bf09ab1b1f63a62a2e1f6caa458
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-01 15:23:12 -0300

    new commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import { ReactDOM } from "react"
 import React from "react"
 import next from "next"
-import { CommonCalculatorBanner, IncomeTaxCalculatorBanner } from "../src/components/calculatorBanner"
+import { CommonCalculatorBanner, ImcCalculatorBanner, IncomeTaxCalculatorBanner } from "../src/components/calculatorBanner"
 
 
 function HomePage() {
     return (
         <>
-            <section className="w-full h-full bg-darkBluePrimary flex justify-center gap-5 items-center p-2">
+            <section className="w-full h-full bg-darkBluePrimary grid justify-center gap-5 items-center p-2">
                 <CommonCalculatorBanner></CommonCalculatorBanner>
                 <IncomeTaxCalculatorBanner></IncomeTaxCalculatorBanner>
+                <ImcCalculatorBanner></ImcCalculatorBanner>
             </section>
         </>
     )
--- src/components/calculatorBanner.js
+++ src/components/calculatorBanner.js
@@ -3,6 +3,7 @@ import Image from "next/image"
 import { useRouter } from "next/router"
 import commonCalculatorIcon from "./img/commonCalculator.svg"
 import incomeTaxCalculatorIcon from "./img/incomeTaxCalculator.svg"
+import imcCalculatorIcon from "./img/imcCalculator.svg"
 
 export function CommonCalculatorBanner() {
     const routes = useRouter()
@@ -25,6 +26,19 @@ export function IncomeTaxCalculatorBanner() {
                 <Image width={'50px'} height={'50px'} src={incomeTaxCalculatorIcon} />
                 <h2 className="text-white font-bold text-2xl">Calculadora de Imposto de Renda</h2>
                 <p className="text-white">Calculadora  com funcionalidade para avaliar sua situação de imposto de renda</p>
+            </div>
+        </button>
+    )     
+}
+
+export function ImcCalculatorBanner() {
+    const routes = useRouter()
+    return (
+        <button type="button" onClick={()=> {routes.push(`/incomeTaxCalculator`)}}>
+            <div className="min-w-[300px] bg-yelloPrimary p-2 flex flex-col gap-2">
+                <Image width={'50px'} height={'50px'} src={imcCalculatorIcon} />
+                <h2 className="text-white font-bold text-2xl">Calculadora de IMC</h2>
+                <p className="text-white">Calculadora que calcula o IMC</p>
             </div>
         </button>
     )     
