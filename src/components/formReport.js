f5381d5ed1f7be7f5d6bde1c622388287647d94d
Ronaldjga <Ronaldalmeida20022003@gmail.com>
2022-03-09 11:47:16 -0300

    reportPage

--- src/components/formReport.js
+++ src/components/formReport.js
@@ -3,27 +3,46 @@ import react from "react";
 export function FormReport() {
     return (
         <form
-            action="Post"
-            className="min-h-[500px] lg:w-2/4 bg-gray-900 flex flex-col gap-5 p-2 border-2 border-yelloPrimary"
+            action="https://formspree.io/f/xvolrylz"
+            method="POST"
+            className=" lg:w-2/4 bg-gray-900 flex flex-col gap-5 p-5 border-2 border-yelloPrimary"
         >
             <input
+                name="Nome"
+                type={"text"}
                 className="p-2"
-                placeholder="Nome"
-            >
-            
-            </input>
+                placeholder="Seu nome"
+            />
 
-            <select className="p-2">
-                <option select disabled>Selecione a Calculadora com o Erro</option>
+            <input
+                name="E-mail"
+                type={"email"}
+                className="p-2"
+                placeholder="Seu e-mail"
+            />
+            
+            <select
+                name="Calculadora"
+                className="p-2">
+                <option selected disabled>Selecione a Calculadora com o Erro</option>
                 <option>Calculadora Comum</option>
+                <option>Calculadora de Importo de Renda (IRRF)</option>
+                <option>Calculadora de Indice de Massa Corporal (IMC)</option>
             </select>
     
             <textarea
+                required
+                name="Mensagem"
+                className="p-2"
                 placeholder="Explique sobre o erro."
-            rows={`12`}
+                rows={`12`}
             >
-
             </textarea>
+            <button
+                type="submit"
+                className="w-2/5 mx-auto py-4 font-bold hover:brightness-110 bg-yelloPrimary">
+                    ENVIAR
+            </button>
         </form>
     )
 }
